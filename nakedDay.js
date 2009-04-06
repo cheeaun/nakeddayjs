6e93a9491e170c7fc75b7882d483c4281c53f514
Lim Chee Aun <cheeaun@gmail.com>
2009-04-06 22:36:21 +0800

    Clean up code and fixed tiny bugs.

--- nakedDay.js
+++ nakedDay.js
@@ -1,21 +1,21 @@
-nakedDayNow = new Date();
-nakedDayStart = new Date(nakedDayNow.getFullYear(),3,9,0,0,0);
-nakedDayEnd = new Date(nakedDayNow.getFullYear(),3,10,0,0,0);
+(function(){
+	var now = new Date();
+	var time = now.getTime();
+	var start = new Date(now.getFullYear(), 3, 9, 0, 0, 0).getTime();
+	var end = new Date(now.getFullYear(), 3, 10, 0, 0, 0).getTime();
 
-if(nakedDayNow.getTime() >= nakedDayStart.getTime() && nakedDayNow.getTime() <= nakedDayEnd.getTime())
-{
-    var nakedDayCSS = document.getElementsByTagName('link');
-    for(var nakedDayI = nakedDayCSS.length; nakedDayI >= 0; nakedDayI--)
-    {
-        if(nakedDayCSS[nakedDayI] && ((nakedDayCSS[nakedDayI].href.indexOf('.css') > -1 || nakedDayCSS[nakedDayI].type == 'text/css')))
-        {
-            nakedDayCSS[nakedDayI].parentNode.removeChild(nakedDayCSS[nakedDayI]);
-        }
-    }
+	if (time >= start && time <= end){
+		var link = document.getElementsByTagName('link');
+		for (var i=0, len=link.length; i<len; i++){
+			var l = link[i];
+			if (l && ((l.href.indexOf('.css') > -1 || l.rel == 'stylesheet'))){
+				l.parentNode.removeChild(l);
+			}
+		}
 
-    var nakedDayCSS = document.getElementsByTagName('style');
-    for(var nakedDayI = nakedDayCSS.length; nakedDayI >= 0; nakedDayI--)
-    {
-        nakedDayCSS[nakedDayI].parentNode.removeChild(nakedDayCSS[nakedDayI]);
-    }
-}
+		var style = document.getElementsByTagName('style');
+		for (var i=0, len=style.length; i<len; i++){
+			style[i].parentNode.removeChild(style[i]);
+		}
+	}
+})();
